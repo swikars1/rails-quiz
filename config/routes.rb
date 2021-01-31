550ab8ceb7887116e2ade802c46d22a7acc1e5df
swikars1 <swikarsharma@gmail.com>
2021-01-31 11:01:16 +0545

    add user session auth and controller

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
